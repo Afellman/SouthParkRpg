d9e2ec122d37275beb2cc47d681ae2f6daf74f5c
Afellman <red.kasid@gmail.com>
2017-12-02 16:23:18 -0500

    finished game (might add resonsivity later)

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,33 +12,9 @@ $(document).ready(function () {
   var charIsChosen = 0;
   var enemyIsChosen = 0;
 
-  var charLeft = 3;
-  //object storing the character data
-  var charObj = {
-    stan: {
-      "name": "Stan", 
-      "ap" : 15,
-      "hp": 115
-    },
-
-    kyle: {
-      "name": "Kyle",
-      "ap" : 12,
-      "hp" : 120
-    },
-
-    kenny: {
-      "name" : "Kenny",
-      "ap" : 20,
-      "hp" : 75
-    },
-
-    cartmen: {
-      "name" : "Cartmen",
-      "ap": 10,
-      "hp": 150
-    },
-  }
+  var charLeft = -1;
+  //object for storing the character data
+  var charObj ={};
   //--------------------------------------------------------------
 
   //function to print characters 1 by 1.
@@ -48,6 +24,9 @@ $(document).ready(function () {
       setTimeout(function () { showText(target, message, index, interval); }, interval);
     }
   }
+
+  //calling the newgame function to start the game.
+  newGame();
   //function to start the game, resetting the page and redrawing the characters.
   function newGame (){
     $(".char-row").empty();
@@ -62,25 +41,24 @@ $(document).ready(function () {
     charObj = {
       stan: {
         "name": "Stan", 
-        "ap" : 15,
-        "hp": 115
+        "ap" : 20,
+        "hp": 110
       },
       
       kyle: {
         "name": "Kyle",
+        "ap" : 15,
+        "hp" : 160
+      },
+      kenny: {
+        "name" : "Kenny",
         "ap" : 10,
         "hp" : 150
       },
-      
-      kenny: {
-        "name" : "Kenny",
-        "ap" : 7,
-        "hp" : 75
-      },
       cartmen: {
         "name" : "Cartmen",
-        "ap": 25,
-        "hp": 100
+        "ap": 30,
+        "hp": 130
         
       },
     }
@@ -91,8 +69,9 @@ $(document).ready(function () {
     for (var key in charObj) {
       console.log(charObj[key])
       console.log(key);
-      var divInsert = '<div id="' + key + '" class="chars char-select rounded center bg-dark text-white">' + key + '<p class="stats">HP: ' + charObj[key]["hp"] + " AP: " +charObj[key]["ap"] + '</div>'
+      var divInsert = '<div id="' + key + '" class="chars char-select rounded center bg-dark text-white">' + key + '<p class="stats">HP: ' + charObj[key]["hp"]+ '</div>';
       $(".char-row").append(divInsert)
+      charLeft++;
     };
     clicks();
   };
@@ -100,7 +79,6 @@ $(document).ready(function () {
 
   //function to listen for and react to clicks
   function clicks() {
-    //listens for a click on a character
     $(".chars").on("click", function () {
       console.log("onclick")
       //sets first click to player and flips player toggle so it can't be chosen again. 
@@ -132,7 +110,7 @@ $(document).ready(function () {
   };
   //--------------------------------------------------------------
 
-    //function to start each round. addes
+    //function to start each round.
    function roundStart() {
     $(function () {
       $('.text-row').empty();
@@ -140,58 +118,79 @@ $(document).ready(function () {
     });
     $(".player").removeClass("char-select");
     $(".enemy").removeClass("char-select");
-    $(".attack").on("click", function(){
-      charObj[player]["hp"] -= charObj[enemy]["ap"];
-      charObj[enemy]["hp"] -= charObj[player]["ap"];
-      charObj[player]["ap"] += playerAp; 
-      $(".player-stats").text('HP: ' + charObj[player]["hp"] + " AP: " +charObj[player]["ap"]);
-      $(".enemy-stats").text('HP: ' + charObj[enemy]["hp"] + " AP: " +charObj[enemy]["ap"]);
-      if (charObj[enemy]["hp"] <= 0) {
-        console.log(enemy);
-        $(".enemy").remove();
-        $(".attack").remove();
-        enemyIsChosen = 0;
-        $(function () {
-          $('.text-row').empty();
-          showText(".text-row", 'Owned!', 0, 40 );
-        });
-        charLeft -= 1;
-        console.log(charLeft);
-        if (charLeft == 0) {
-          console.log("wingame");
-          setTimeout(function() { 
-            winGame()}
-          , 800);
-          
-        }
-        else {
-          setTimeout(function() { 
-            $(function () {
-              $('.text-row').empty();
-              showText(".text-row", 'Choose Your Next Enemy!', 0, 40 );
-            });}
-          , 800);
-        clicks();
-        };
- 
+    $(".attack").on("click", attack);
+  };
+
+  //--------------------------------------------------------------
+  
+  //function to subtract player and enemy HP and increase player AP
+  function attack() {
+    console.log("attack")
+    $('.text-row').empty();
+    charObj[player]["hp"] -= charObj[enemy]["ap"];
+    charObj[enemy]["hp"] -= charObj[player]["ap"];
+    $(".text-row").append('<h2>You attacked '+ enemy + ' for ' + charObj[player]["ap"] + ' points');
+    $(".text-row").append('<h2>' + enemy + ' attacked you for ' + charObj[enemy]["ap"] + ' points');
+    charObj[player]["ap"] += playerAp; 
+    $(".player-stats").text('HP: ' + charObj[player]["hp"]);
+    $(".enemy-stats").text('HP: ' + charObj[enemy]["hp"]);
+
+    //if statemment to check if player or enemy is dead
+    if (charObj[player]["hp"] <= 0){
+      console.log("player is dead");
+      loseGame();
+    }
+    else if (charObj[enemy]["hp"] <= 0) {
+      console.log("hp" + charObj[player]['hp']);
+      console.log(enemy +"is dead");
+      $(".enemy").remove();
+      $(".attack").remove();
+      enemyIsChosen = 0;
+      $(function () {
+        $('.text-row').empty();
+        showText(".text-row", 'Owned!', 0, 40 );
+      });
+      charLeft -= 1;
+      console.log(charLeft);
+
+      //if statement to check if the game is over or move on to the next enemy
+      if (charLeft == 0) {
+        console.log("wingame");
+        setTimeout(function() { 
+          winGame()}
+        , 800);
         
       }
-      else if (charObj[player]["hp"] <= 0){
-        loseGame();
-      }
-      
-    });
-  };
-  //--------------------------------------------------------------
+      else {
+        setTimeout(function() { 
+          $(function () {
+            $('.text-row').empty();
+            showText(".text-row", 'Choose Your Next Enemy!', 0, 40 );
+          });}
+        , 800);
+      clicks();
+      };
 
+      
+    }
+
+  };
+  
   //function for tell the user that they lost and ask if they want to play again
   function loseGame(){
-    $(".text-row").html('<h1>You Lose...</h1>');
+    $(".text-row").empty();
+    $(".fight-row").empty();
+    $(".char-row").empty();
+    $(function () {  $(".text-row").empty();
+    showText(".text-row", "You Lose...", 0, 40);
+  });
     setTimeout(function() {
       $(".text-row").empty();
-      $(".text-row").append("<button class='play-again btn btn-primary p-3'>Play Again");
+      $(".text-row").append("Game Over");
+      // $(".fight-row").addClass("justify-content-center");
+      $(".fight-row").append("<button class='play-again btn btn-primary p-3 m-auto'>Play Again");
       $(".play-again").on('click', newGame);
-    },800
+    }, 2000
     );
   }
 
@@ -209,5 +208,5 @@ $(document).ready(function () {
     
 
   }
-  newGame();
+
 });
